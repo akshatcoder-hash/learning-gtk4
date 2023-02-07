2efd0758e49bd495f8867ef44c6d22a083f2d9d2
Akshat Sharma <akshatcoder@outlook.com>
2023-02-07 21:53:51 +0530

    👋 Hello gtk

--- hello-gtk/src/main.rs
+++ hello-gtk/src/main.rs
@@ -1,3 +1,12 @@
+use gtk::prelude::*;
+use gtk::Application;
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld1";
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Run the application
+    app.run();
 }
